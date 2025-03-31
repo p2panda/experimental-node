c6f0f60c1428e86c5c66d1c3e6e37d2ff8ed0c8c
Sam Andreae <contact@samandreae.com>
2025-03-31 15:50:21 +0100

    Add table to migration file

--- migrations/20250331144110_acked-operations.sql
+++ migrations/20250331144110_acked-operations.sql
@@ -1,1 +1,6 @@
--- Add migration script here
+CREATE TABLE IF NOT EXISTS acked_v1 (
+    public_key              TEXT            NOT NULL,
+    log_id                  TEXT            NOT NULL,
+    seq_num                 TEXT            NOT NULL,
+    PRIMARY_KEY(public_key, log_id)
+);
